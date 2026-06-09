1edd842ee56898e62d282cd6d193c4561fc3d46a
mai anh <vumaianh2006tbkx@gmail.com>
2026-06-09 08:30:54 +0700

    [FEATURE] Add custom FizzBuzz

--- PBT_07/fizzbuzz.js
+++ PBT_07/fizzbuzz.js
@@ -7,3 +7,27 @@ for (let i = 1; i <= 100; i++) {
     else console.log(i);
 }
 
+// Version 2: Custom FizzBuzz linh hoạt dữ liệu
+console.log("\n--- CUSTOM FIZZBUZZ ---");
+function customFizzBuzz(n, rules) {
+    for (let i = 1; i <= n; i++) {
+        let output = "";
+        
+        // Duyệt qua từng quy tắc được truyền vào cấu trúc luật định
+        for (let j = 0; j < rules.length; j++) {
+            if (i % rules[j].divisor === 0) {
+                output += rules[j].word;
+            }
+        }
+        
+        // Nếu không thỏa mãn bất kì chia hết nào, xuất chính số đó
+        console.log(output === "" ? i : output);
+    }
+}
+
+// Chạy test case đề bài yêu cầu
+customFizzBuzz(35, [
+    { divisor: 3, word: "Fizz" },
+    { divisor: 5, word: "Buzz" },
+    { divisor: 7, word: "Jazz" }
+]);
